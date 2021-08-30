8a6e81973286c17a68230879417d28b0742abc7d
nobaby31 <89760341+nobaby31@users.noreply.github.com>
2021-08-30 10:33:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'hi lo e iu à!',
+    introDesc: `E là người con gái a iu nhất sau mẹ e <33
+    E sinh đẹp nhất sau mẹ a  <33  
+    E cx đáng iu nhất sau mẹ a nốt! 
+    E là điều may mắn trong cuộc sống của a ^^
+    Có e, a sẽ chẳng cần điều gì khác( Ngoại trừ mẹ)  `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Ở bên e là lời tỏ tình lâu dài nhất. Dù già hay ốm, a vẫn sẽ sẵn sàng ở bên em! <33 ',
+    desc: 'Hôm nay a sẽ ngõ lời tỏ tình chính thức nhất luôn , e có iu 1 người đáng iu như a hông, hãy luôn bên a nha  ',
+    btnYes: 'Dạ e đồng ý , iu a mãi luôn <33',
+    btnNo: 'Không, a thật là đáng ghét   à :3',
+    question:'Lí do gì khiến e có thể iu a, trên thế giới có hơn 7 tỉ người mà sao e lại chọn riêng a <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Tại vì bên a e chỉ mún yêu, iu a e kh cần lí do, và a là 1 người tuyệt vời nhất đối với e rùi,đáng iu và đẹp trai nữa  <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều, a sẽ luôn bên e dù khó khăn nhất  😘😘',
+    messDesc: 'Tối nay a sẽ gửi cho e vitamin nhé công chúa đáng iu của a.',
+    btnAccept: 'Okiiiii lun, cảm ơn a nha, iu a suốt đời  <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
